a374baeeed50b3be10d6e6858f331767b956a40d
LovesLC <j.silva2710@gmail.com>
2024-07-02 12:19:11 -0300

    Upload Mvc

---
+++ LanchesMac/Models/Categoria.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace LanchesMac.Models
+{
+	public class Categoria
+	{
+		
+		public int CategoriaId { get; set; }
+		public string CategoriaNome { get; set; }
+        public string Descrição { get; set; }
+
+		//para identificar o relacionamento entre categoria e lanches
+		public List<Lanche> Lanches { get; set; }
+
+
+}
+	
+}
+
+
---
+++ LanchesMac/Models/Lanche.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace LanchesMac.Models
+{
+	public class Lanche
+	{
+		public int LancheId { get; set; }
+        public string Nome { get; set; }
+        public string DescricaoCurta { get; set; }
+		public string DescricaoDetalhada{ get; set; }
+		public decimal Preco { get; set; }
+		public string ImagemUrl { get; set; }
+        public string ImagemThumbnailUrl{ get; set; }
+        public bool IsLanchePreferido { get; set; }
+        public bool EmEstoque { get; set; }
+
+        //relacionamento entre as entidades
+
+        //lanche e categoria
+        //chave estrangeira
+        public int CategoriaId { get; set; }
+
+
+
+
+
+}
+}
+
